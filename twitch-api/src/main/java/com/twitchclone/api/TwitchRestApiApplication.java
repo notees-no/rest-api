d55beced65ebc21c545bd883cf81722de374200b
notees-no <skrebloegor@gmail.com>
2024-11-17 14:20:21 +0300

    create project

---
+++ twitch-api/src/main/java/com/twitchclone/api/TwitchRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.twitchclone.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchRestApiApplication.class, args);
+	}
+
+}
---
+++ twitch-api/src/test/java/com/twitchclone/api/TwitchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitchclone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
